f04e2512cffbd4bd5422ad41ef95c469e5b2bb75
Luis Santana <lsantana30@protonmail.com>
2019-04-12 10:28:15 -0700

    Fixed fetch to online server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 
-fetch("/homes", {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json"
+fetch(
+  "http://zallosimilarhomesservice-env.3cy6gkds47.us-east-2.elasticbeanstalk.com/homes",
+  {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
   }
-})
+)
   .then(response => response.json())
   .then(myJson => {
     myJson.forEach(home => Object.assign(home, { saved: false }));
